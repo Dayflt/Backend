2105f325062f34b4f102f964fd0aae27f2f81556
hanee <hanee15@ewhain.net>
2021-07-13 19:40:26 +0900

    add static/swagger.json

--- Backend/web/__init__.py
+++ Backend/web/__init__.py
@@ -5,12 +5,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 import errors as errors
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 api = Api(app, errors = errors)
 app.config.from_pyfile('config.py')
 db=SQLAlchemy(app)
 
+SWAGGER_URL='/swagger'
+API_URL='/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "DAYFLY-Flask-REST-MIXEDVIDEO"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 from web import routes
 
