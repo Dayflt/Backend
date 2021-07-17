cc50eb363f5abd6764f762f1488b97e6b7fb3ebd
Seoyeon Kim <2018103585@khu.ac.kr>
2021-07-17 16:56:30 +0900

    Auto stash before merge of "main" and "origin/main"

--- Backend/web/config.py
+++ Backend/web/config.py
@@ -2,6 +2,6 @@ from web import app
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY='thisisfirstflaskapp'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:1111@localhost/video'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:sylucy12@localhost:5433/video'
 
 FLASK_APP = '__init__.py'
