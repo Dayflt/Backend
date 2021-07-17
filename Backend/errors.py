f81ebacd49fd87b13a3801d838178a6743ff6dd4
harloxx <chloe0105@naver.com>
2021-07-18 00:47:30 +0900

    handle circular import error in errors.py

--- Backend/errors.py
+++ Backend/errors.py
@@ -8,20 +8,12 @@ errors = {
     }
 }
 
-from web import app
 from flask import jsonify
-
 class API_model_id(Exception):
     pass
-@app.errorhandler(500)
-def error(e):
-    return jsonify({
-        "success":False,
-        "message":"Something wrong"
-    })
-@app.errorhandler(API_model_id)
+
 def error2(e):
     return jsonify({
         "success":False,
-        "message":"문제 발생"
+        "message":"문발생"
     })
