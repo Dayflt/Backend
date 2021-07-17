d2f5742dd3cf114bff0a875fc40034f9a843ba2f
harloxx <chloe0105@naver.com>
2021-07-18 00:36:09 +0900

    move class (API_model_id) to errors.py

--- Backend/errors.py
+++ Backend/errors.py
@@ -7,3 +7,21 @@ errors = {
         "status" : 500
     }
 }
+
+from web import app
+from flask import jsonify
+
+class API_model_id(Exception):
+    pass
+@app.errorhandler(500)
+def error(e):
+    return jsonify({
+        "success":False,
+        "message":"Something wrong"
+    })
+@app.errorhandler(API_model_id)
+def error2(e):
+    return jsonify({
+        "success":False,
+        "message":"문제 발생"
+    })
