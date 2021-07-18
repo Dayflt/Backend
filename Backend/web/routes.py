192b7cbd5232e7949a7aa7999df709a8ecf03bf9
harloxx <chloe0105@naver.com>
2021-07-18 16:29:45 +0900

    api5 filtering category_no value

--- Backend/web/routes.py
+++ Backend/web/routes.py
@@ -102,22 +102,28 @@ def return_result(model_id):
 
 @app.route('/api/model/gallery/<category_no>', methods = ['GET'])
 def getby_emoji(category_no):
+    if category_no>4 or category_no<0:
+        abort(500,"category_no is wrong")
     try:
         datas = views.post_gallery_category(category_no) #list형태로 반환
         result = []
         num = len(datas)
         if num == 0:
-            return jsonify({'success' : False})
-        if num < 4:
-            for n in range(num):
-                video = datas[n]
-                print(video)
-                result.append(video.serialize())
-        else:
-            post_gallery(category_no)
-            for n in range(4):
-                video = datas[n]
-                result.append(video.serialize())
+            return jsonify({
+                'success' : True,
+                'message':'No values in request category_no'
+            })
+        #if num < 4:
+        #    for n in range(num):
+        #        video = datas[n]
+        #        print(video)
+        #        result.append(video.serialize())
+        #else:
+        post_gallery(category_no)
+        for n in range(4):
+            video = datas[n]
+            result.append(video.serialize())
+            print(result)
         return json.dumps(result)
     except:
         abort(500, "something wrong in database")
