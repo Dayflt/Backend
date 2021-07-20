1c8c54bf2730eaa07a735729b92c6413190b9f93
Seoyeon Kim <2018103585@khu.ac.kr>
2021-07-20 15:22:14 +0900

    move files outside the Backend folder

--- web/config.py
+++ web/config.py
@@ -1,11 +1,10 @@
 from web import app
-PORT= '5432'
+PORT= '5433'
 USERNAME = 'postgres'
-PASSWORD = '1111'
+PASSWORD = 'sylucy12'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY='thisisfirstflaskapp'
-SQLALCHEMY_DATABASE_URI='postgresql://' + USERNAME + ':' + PASSWORD + '@localhost:+' + PORT + '/video'
 SQLALCHEMY_DATABASE_URI='postgresql://%s:%s@localhost:%s/video'%(USERNAME,PASSWORD,PORT)
 
 FLASK_APP = '__init__.py'
